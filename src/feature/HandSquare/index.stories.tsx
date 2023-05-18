effa41182cee49e0ddd7e7bd4d5b66eec2cf446a
nebular-lab <hiranodaisuke0721@gmail.com>
2023-05-18 13:57:49 +0900

    feat: rangeSquare

--- src/feature/HandSquare/index.stories.tsx
+++ src/feature/HandSquare/index.stories.tsx
@@ -3,27 +3,30 @@ import { HandSquare } from '.';
 type Story = StoryObj<typeof HandSquare>;
 const meta: Meta<typeof HandSquare> = {
   component: HandSquare,
-  argTypes:{
-    
-  }
+  argTypes: {},
 };
 export default meta;
 export const Default: Story = {
   args: {
     handleDraw: () => {},
     suit: 'pair',
+    text: 'AA',
+    isExist: true,
     hands: [
       {
-        actions: [{ move: 'FOLD', percent: 100 }],
+        actions: [{ move: 'FOLD', percent: 10 }],
       },
       {
-        actions: [{ move: 'FOLD', percent: 100 }],
+        actions: [
+          { move: 'FOLD', percent: 10 },
+          { move: 'CALL', percent: 10 },
+        ],
       },
       {
-        actions: [{ move: 'FOLD', percent: 100 }],
+        actions: [{ move: 'FOLD', percent: 10 }],
       },
       {
-        actions: [{ move: 'FOLD', percent: 100 }],
+        actions: [{ move: 'BET M', percent: 100 }],
       },
       {
         actions: [{ move: 'FOLD', percent: 100 }],
--- src/feature/HandSquare/index.tsx
+++ src/feature/HandSquare/index.tsx
@@ -1,5 +1,5 @@
 import { ActionType, HandType, suitType } from '@/types';
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, SimpleGrid, Text, grid } from '@chakra-ui/react';
 import { FC } from 'react';
 type Props = {
   handleDraw: () => void;
@@ -12,19 +12,39 @@ export const HandSquare: FC<Props> = (props) => {
   const { handleDraw, suit, text, hands, isExist } = props;
   const boxSize = '60px';
   const textColor = isExist ? 'white' : 'gray.400';
+  let gridNum: number;
+  switch (suit) {
+    case 'pair':
+      gridNum = 6;
+      break;
+    case 'suited':
+      gridNum = 4;
+      break;
+    case 'offsuited':
+      gridNum = 12;
+      break;
+  }
   return (
     <Flex
       w={boxSize}
       h={boxSize}
-      bg={'red'}
+      bg={'main'}
       position={'relative'}
       _hover={{ cursor: 'pointer' }}
       justifyContent={'center'}
       alignItems={'center'}
       border={'1px'}
+      boxSizing="border-box"
     >
+      <SimpleGrid columns={gridNum} position={'absolute'} w={'full'} h={'full'}>
+        {hands.map((hand) => {
+          return <HandStripe actions={hand.actions} suit={suit} />;
+        })}
+      </SimpleGrid>
+      <Text position={'absolute'} userSelect={'none'} color={textColor}>
+        {text}
+      </Text>
       <Mask />
-      <Text userSelect={'none'} color={textColor}>{text}</Text>
     </Flex>
   );
 };
@@ -39,5 +59,47 @@ const Mask = () => {
       _hover={{ opacity: 1.0 }}
       position={'absolute'}
     ></Flex>
+  );
+};
+type HandStripeProps = {
+  actions: ActionType[];
+  suit: suitType;
+};
+const HandStripe: FC<HandStripeProps> = (props) => {
+  const { actions, suit } = props;
+
+  return (
+    <Flex direction={'column-reverse'} w={'full'} h={'full'}>
+      {actions.map((action) => {
+        let bg = 'main';
+        switch (action.move) {
+          case 'FOLD':
+            bg = 'fold';
+            break;
+          case 'CALL':
+            bg = 'call';
+            break;
+          case 'RAISE':
+            bg = 'raise';
+            break;
+          case 'ALLIN':
+            bg = 'allin';
+            break;
+          case 'CHECK':
+            bg = 'check';
+            break;
+          case 'BET S':
+            bg = 'betS';
+            break;
+          case 'BET M':
+            bg = 'betM';
+            break;
+          case 'BET L':
+            bg = 'betL';
+            break;
+        }
+        return <Box w={'full'} h={`${action.percent}%`} bg={bg}></Box>;
+      })}
+    </Flex>
   );
 };
