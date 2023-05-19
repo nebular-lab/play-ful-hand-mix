0f247e957f97fc8f2a3ad56eb94da34297771158
nebular-lab <hiranodaisuke0721@gmail.com>
2023-05-19 17:43:26 +0900

    delete: storybook file

--- src/common/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@chakra-ui/react';
-import { FC } from 'react';
-type Props = {
-  label: string;
-};
-export const CustomButton: FC<Props> = (props) => {
-  const { label } = props;
-  return <Button>{label}</Button>;
-};
